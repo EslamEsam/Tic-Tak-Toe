51fd2a8300a53f320df40914cf1318e5cc6270fa
Eslam_Esam <me@eslamesam.com>
2020-09-22 01:11:31 +0200

    first commit

--- main.c
+++ main.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//This program Prints the perfect numbers from 1 to 1000
+//This program converts Dollar to Egyptian Pound
+
 int main()
 {
-    int sum = 0 ;
-for(int i = 1 ; i <= 1000 ; i++){
-        sum = 0;
-    for(int j = 1 ; j < i ; j++){
-        if(i%j==0){
-            sum += j;
-        }
-    }
-    if(sum == i){
-        printf (" %d",i);
-        printf ("-> perfect number \n");
-    }
-}
-return 0;
+   float Dollar_Price,Dollars,Result;
+   printf("Enter today's Dollar price = ");
+   scanf("%f",&Dollar_Price);
+   printf("Enter the amount of money you want to convert = ");
+   scanf("%f",&Dollars);
+
+   Result=Dollar_Price*Dollars;
+
+   printf("Your money = %f",Result);
+
 }
