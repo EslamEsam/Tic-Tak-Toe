0b5bf71f93b63c76d19f14bb9cdf3143b9fb8b44
Eslam_Esam <me@eslamesam.com>
2020-09-25 12:41:28 +0200

    first commit

--- main.c
+++ main.c
@@ -3,44 +3,25 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-//this program reads 20 number from the user and display the unique ones and their sum
+//This is a Tic Tac Toe game made with C language
 
-int sum(int arr[]){
-    int sum = 0 ;
-for(int i = 0 ; i < 20 ; i++){
-    sum += arr[i];
+void isWinning (){
+
 }
-return sum;
+
+void insert (){
+
 }
+
+void Print (){
+
+}
+
+
 
 int main()
 {
-bool check=false;
-int num,result;
-int arr[20]={};
-printf("Enter the numbers : ");
-for(int i = 0 ; i < 20 ;i++){       //here to get the numbers and check if it exits or not
-    check = false;
-    scanf(" %d",&num);
-    for(int j = 0 ; j < 20 ; j++){
-        if(num==arr[j])
-            check = true;
-
-    }
-if(check==false)
-    arr[i]=num;
-
-}
-
-printf("The unique number are :");
-for(int i = 0 ; i < 20 ; i++){
-if (arr[i]==0)
-    continue;
-else
-    printf("%d \n",arr[i]);
-}
-result = sum (arr);
-printf("Their sum = %d",result);
+int arr[3][3]={};
 
 return 0;
 }
