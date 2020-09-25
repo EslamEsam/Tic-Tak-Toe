9d5cfa827f33220937c8142f2558f62c3e56f3e4
omnia451 <59293260+omnia451@users.noreply.github.com>
2020-09-25 13:14:46 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,27 +1,29 @@
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-
-//This is a Tic Tac Toe game made with C language
-
-void isWinning (){
-
-}
-
-void insert (){
-
-}
-
-void Print (){
-
-}
-
-
 
 int main()
 {
-int arr[3][3]={};
-
-return 0;
+    int number;
+    int i;
+    int sum = 0;
+    printf("Enter a number!\n");
+    scanf(" %d", &number);
+    if (number > 1){
+        for (i = 1; i < number; i++){
+        if (number % i == 0){
+            sum = sum + i;
+            }
+        }
+        if (number == sum){
+            printf("%d is a perfect number\n", number);
+        }
+        else{
+            printf("%d is not a perfect number\n", number);
+        }
+    }
+    else{
+        printf("%d is not a perfect number\n", number);
+    }
+    main();
+    return 0;
 }
